9bb450dfb0d1247fb7d0b2292a7af9bd74a34d5c
Sam Dodrill <xena@yolo-swag.com>
2015-02-23 22:31:56 -0800

    Fix quoting issue for defined()
    Thanks @xaquseg

--- clientlibs/php/krewella.php
+++ clientlibs/php/krewella.php
@@ -3,7 +3,7 @@
 // a bool, string pair describing if it worked and why not if it didn't.
 // This figures the host and key out based on a define() or an envvar for you.
 function krewella_message($network, $channel, $message) {
-	if(!defined(KREWELLA_HOST)) {
+	if(!defined('KREWELLA_HOST')) {
 		if(!getenv('KREWELLA_HOST')) {
 			return array(FALSE, 'No host defined');
 		} else {
@@ -13,7 +13,7 @@ function krewella_message($network, $channel, $message) {
 		$host = constant('KREWELLA_HOST');
 	}
 
-	if(!defined(KREWELLA_KEY)) {
+	if(!defined('KREWELLA_KEY')) {
 		if(!getenv('KREWELLA_KEY')) {
 			return array(FALSE, 'No key defined');
 		} else {
