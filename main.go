5b60582b7be0cd6cea5ece2add2c9098011fe6f9
Sam Dodrill <xena@yolo-swag.com>
2015-02-23 20:02:38 -0800

    main: set up the world, die if it's bad

--- main.go
+++ main.go
@@ -1,10 +1,15 @@
+/*
+Command krewella is an HTTP POST -> IRC message gateway.
+*/
 package main
 
 import (
 	"fmt"
 	"net/http"
 	"os"
+	"strings"
 
+	"christine.website/go/krewella/irc"
 	"github.com/codegangsta/negroni"
 	"github.com/drone/routes"
 )
@@ -12,9 +17,41 @@ import (
 var (
 	router *routes.RouteMux
 	n      *negroni.Negroni
+
+	bots map[string]*irc.Bot
 )
 
+func createBots() error {
+	bots = make(map[string]*irc.Bot)
+
+	networklist := os.Getenv("KREWELLA_NETWORKS")
+	networks := strings.Split(networklist, ",")
+
+	for _, network := range networks {
+		bot, err := irc.New(network)
+		if err != nil {
+			return err
+		}
+
+		bots[strings.ToLower(network)] = bot
+	}
+
+	return nil
+}
+
+func destroyBots() {
+	for _, bot := range bots {
+		bot.IrcObj.Quit()
+	}
+}
+
 func main() {
+	err := createBots()
+	if err != nil {
+		panic(err)
+	}
+	defer destroyBots()
+
 	router = routes.New()
 
 	router.Get("/", func(w http.ResponseWriter, req *http.Request) {
