d49dd8bd9f553eec567fe58d5be1815940ac6e45
Sam Dodrill <xena@yolo-swag.com>
2015-02-23 22:55:30 -0800

    PHP Library: add documentation and exceptions
    Thanks @xaquesg and @Mihail-K for giving convincing arguments as to why
    a one function and one file API needs to have exceptions instead of
    using multiple returnsllike sane programmers should use!

--- clientlibs/php/krewella.php
+++ clientlibs/php/krewella.php
@@ -1,11 +1,52 @@
 <?php
-// krewella_message takes in a network, channel and message string and returns
-// a bool, string pair describing if it worked and why not if it didn't.
-// This figures the host and key out based on a define() or an envvar for you.
+
+/**
+ * Base Krewella Exception because apparently one-file one-function libraries need them.
+ */
+class KrewellaBaseException extends Exception {};
+
+/**
+ * Exception but with details. Takes and holds a code and a message.
+ * If you make code anything but a number and message anything but a string you deserve whatever breaks.
+ */
+class KrewellaDetailedException extends KrewellaBaseException {
+	public $code;
+	public $message;
+
+	public function __construct($code, $message) {
+		$this->code = $code;
+		$this->message = $message;
+	}
+};
+
+/**
+ * Thrown if we have no hostname to use. You need to have http as part of it or things will break.
+ */
+class KrewellaNoHostException extends KrewellaBaseException {};
+
+/**
+ * Thrown if we have no API key.
+ */
+class KrewellaNoAPIKeyException extends KrewellaBaseException {};
+
+/**
+ * Thrown if Krewella says it doesn't know that network.
+ */
+class KrewellaNoSuchNetworkException extends KrewellaBaseException {};
+
+/**
+ * Thrown if some real weid crap happened.
+ */
+class KrewellaSomeRealWeirdCrapHappenedException extends KrewellaDetailedException {};
+
+/**
+ * Take in a network name, channel name and message string, Return true or throw one of the above
+ * exceptions if something abormal happens.
+ */
 function krewella_message($network, $channel, $message) {
 	if(!defined('KREWELLA_HOST')) {
 		if(!getenv('KREWELLA_HOST')) {
-			return array(FALSE, 'No host defined');
+			throw new KrewellaNoHostException();
 		} else {
 			$host = getenv('KREWELLA_HOST');
 		}
@@ -15,7 +56,7 @@ function krewella_message($network, $channel, $message) {
 
 	if(!defined('KREWELLA_KEY')) {
 		if(!getenv('KREWELLA_KEY')) {
-			return array(FALSE, 'No key defined');
+			throw new KrewellaNoAPIKeyException();
 		} else {
 			$key = getenv('KREWELLA_KEY');
 		}
@@ -23,7 +64,7 @@ function krewella_message($network, $channel, $message) {
 		$key = constant('KREWELLA_KEY');
 	}
 
-	$ch = curl_init($host . '/' . $network . '/' . $channel);
+	$ch = curl_init($host . '/message/' . $network . '/' . $channel);
 
 	curl_setopt($ch, CURLOPT_HTTPHEADER, array(
 		"X-Krewella-Auth: $key",
@@ -36,7 +77,13 @@ function krewella_message($network, $channel, $message) {
 	$reply = curl_exec($ch);
 	$code = curl_getinfo($ch, CURLINFO_HTTP_CODE);
 
+	if ($code == 404) {
+		throw new KrewellaNoSuchNetworkException();
+	} elseif ($code != 200) {
+		throw new KrewellaSomeRealWeirdCrapHappenedException($code, $message);
+	}
+
 	curl_close($ch);
 
-	return array($code == 200, $reply);
+	return TRUE;
 }
